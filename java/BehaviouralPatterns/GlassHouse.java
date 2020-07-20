7f517b61992ade1d0c40c12415edfbc7595f8faa
Kasaragadda Sunitha <kasaragaddasunitha@gmail.com>
2020-07-20 12:34:50 +0530

    first commit

---
+++ java/BehaviouralPatterns/GlassHouse.java
@@ -0,0 +1,15 @@
+package BehaviouralPatterns;
+
+public class GlassHouse extends HouseTemplate{
+	@Override
+	public void buildWall() {
+		System.out.println("Building Glass Walls");
+	}
+
+	@Override
+	public void buildPillar() {
+		System.out.println("Building Pillar with glass coating");
+	}
+
+}
+b
